a0b49866aed52cb2feb112710f51a1d7f7ed30fb
jingaxing <xingj@google.com>
2016-05-22 17:38:20 -0500

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,24 @@
 //define functions here
 
+function getIt(){
+  alert("Hey!");
+}
+function frameIt(){
+  $(this).addClass("tasty");
+}
+function pressIt(){
+  alert("You pressed g key!");
+}
+function submitIt(){
+  alert("your form is going to be submitted now");
+  return;
+}
+
 $(document).ready(function(){
 
 // call functions here
-
+$("p").click(getIt);
+$("img").load(frameIt);
+$("input").keydown(pressIt);
+$("form").submit(submitIt);
 });
